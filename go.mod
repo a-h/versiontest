c283bd97c1b0aa3722b456be4124c51f12b29946
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-01-07 08:44:00 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/a-h/versiontest
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+func main() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("No version")
+		os.Exit(1)
+	}
+	fmt.Println(info.Main.Version)
+}
